0e9150e307d1c96c3729c71c2894efdd6327eacf
Anna Radchenko <anrd.rs>
2022-04-25 18:39:30 +0300

    sumSalary function added

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+    
+    for (let salary in salaries) {
+        if ( typeof salaries[salary] === 'number' && isFinite(salaries[salary]) ) {
+            result += salaries[salary];
+        }
+    }
+    return result;
 }
