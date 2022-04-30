cf1b4ab1dfc4231b3dc0d16dfcca95769c8f4a79
Anna Radchenko <anrd.rs>
2022-04-30 18:40:21 +0300

    camelize function added

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let splitedStr = str.split('');
+
+  for(let i = 0; i < splitedStr.length; i++) {
+      let index = splitedStr.findIndex(item => item === '-');
+      let replacement = splitedStr[index + 1].toUpperCase();
+
+      if (index === -1) {
+          break;
+      }
+      splitedStr.splice(index, 2, replacement);
+  }
+  return splitedStr.join('');
 }
