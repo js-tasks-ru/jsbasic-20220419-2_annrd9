09f22bf4b1534c669566857218617a249c80fd96
Anna Radchenko <anrd.rs>
2022-05-15 23:44:53 +0300

    function toggleText added

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('button');
+  let text = document.querySelector('#text');
+
+  button.addEventListener('click', () => {
+      text.toggleAttribute('hidden')
+  })
 }
